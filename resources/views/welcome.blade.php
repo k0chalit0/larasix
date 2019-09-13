af660363c8a20eb0eeaed21c226be462ecf43ad9
Jose Iriarte <iriarte.joselito@gmail.com>
2019-09-13 05:32:27 -0400

    change Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Practica Devops con Laravel J4H1
                 </div>
 
                 <div class="links">
